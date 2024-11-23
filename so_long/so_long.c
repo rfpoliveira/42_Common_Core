73ec7641b61ae76fddfd6192adfd4ee3c4562d94
Renato Francisco Pedrosa De Oliveira <rpedrosa@c1r5s6.42porto.com>
2024-11-23 14:11:38 +0000

    00

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -12,25 +12,26 @@
 #include "minilibx-linux/mlx.h"
 #include "/usr/include/X11/keysymdef.h"
 #include <unistd.h>
+#include <stdio.h>
 
 typedef struct	s_vars {
 	void	*mlx;
 	void	*win;
 }				t_vars;
-
-int	r_close(int keycode, t_vars *vars)
+int mae(int keycode, t_vars *vars)
 {
 	if (keycode == 0xff1b)
 		mlx_destroy_window(vars->mlx, vars->win);
+	if (keycode == 0xff09)
+		write(1, "hello", 6);
 	return (0);
 }
 
-/*int print(int keycode)*/
-/*{*/
-/*	if (keycode == )*/
-/*		write(1, "hello", 6);*/
-/*	return (0);*/
-/*}*/
+int printfhello()
+{
+	printf("hello");
+	return (0);
+}
 
 int	main(void)
 {
@@ -38,6 +39,8 @@ int	main(void)
 
 	vars.mlx = mlx_init();
 	vars.win = mlx_new_window(vars.mlx, 640, 320, "Hello world!");
-	mlx_hook(vars.win, 2, 1L<<0, r_close, &vars);
+
+//	mlx_hook(vars.win, 2, 1L<<0, mae, &vars);
+	mlx_hook(vars.win , 6, 1L<<4, printfhello, &vars);
 	mlx_loop(vars.mlx);
 }
