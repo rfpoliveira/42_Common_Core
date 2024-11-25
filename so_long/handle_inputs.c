868a5adc0b60bc606386ed1c960f6b5d8bd41fe3
Renato Francisco Pedrosa De Oliveira <rpedrosa@c1r5s7.42porto.com>
2024-11-25 17:45:28 +0000

    00

--- so_long/handle_inputs.c
+++ so_long/handle_inputs.c
@@ -1,31 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.c                                          :+:      :+:    :+:   */
+/*   handle_inputs.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpedrosa <rpedrosa@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/20 11:52:48 by rpedrosa          #+#    #+#             */
-/*   Updated: 2024/11/20 11:55:14 by rpedrosa         ###   ########.fr       */
+/*   Created: 2024/11/25 14:32:40 by rpedrosa          #+#    #+#             */
+/*   Updated: 2024/11/25 14:33:48 by rpedrosa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "so_long.h"
 
-int	main(void)
+int handle_input(int keycode, t_vars *vars)
 {
-	t_vars	vars;
-
-	vars.mlx = mlx_init();
-	if (!vars.mlx)
-		return (1);
-	vars.win = mlx_new_window(vars.mlx, HEIGHT, WIDTH, "Hello world!");
-	if (!vars.win)
+	if (keycode == XK_Escape)
 	{
-		mlx_destroy_display(vars.mlx);
-		free(vars.mlx);
-		return (1);
+		mlx_destroy_window(vars->mlx, vars->win);
+		mlx_destroy_display(vars->mlx); 
+		free(vars->mlx);
+		exit(0);
 	}
-	mlx_hook(vars.win, 2, 1L<<0, handle_input, &vars);
-	//mlx_hook(vars.win , 6, 1L<<4, printfhello, &vars);
-	mlx_loop(vars.mlx);
+	if (keycode == XK_space)
+		ft_printf("hello");
+	return (0);
 }
+
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -1,31 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.c                                          :+:      :+:    :+:   */
+/*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpedrosa <rpedrosa@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/20 11:52:48 by rpedrosa          #+#    #+#             */
-/*   Updated: 2024/11/20 11:55:14 by rpedrosa         ###   ########.fr       */
+/*   Created: 2024/11/25 14:24:17 by rpedrosa          #+#    #+#             */
+/*   Updated: 2024/11/25 14:27:38 by rpedrosa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "so_long.h"
 
-int	main(void)
-{
-	t_vars	vars;
+#ifndef SO_LONG_H
+# define SO_LONG_H
 
-	vars.mlx = mlx_init();
-	if (!vars.mlx)
-		return (1);
-	vars.win = mlx_new_window(vars.mlx, HEIGHT, WIDTH, "Hello world!");
-	if (!vars.win)
-	{
-		mlx_destroy_display(vars.mlx);
-		free(vars.mlx);
-		return (1);
-	}
-	mlx_hook(vars.win, 2, 1L<<0, handle_input, &vars);
-	//mlx_hook(vars.win , 6, 1L<<4, printfhello, &vars);
-	mlx_loop(vars.mlx);
-}
+// libraries
+
+#include "minilibx-linux/mlx.h"
+#include <X11/keysym.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "libft/libft.h"
+
+// macros
+
+#define HEIGHT 640
+#define WIDTH 320
+
+// custom structs
+
+typedef struct s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+// functions
+
+int	handle_input(int keycode, t_vars *vars);
+
+#endif
